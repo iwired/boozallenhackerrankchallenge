1335bdbf7507983e60a18c11b9519bfd0041e40c
iwired <34634398+iwired@users.noreply.github.com>
2018-01-10 17:19:40 -0500

    Add code via upload

---
+++ StringSimilarity.java
@@ -0,0 +1,27 @@
+package boozallen;
+
+public class StringSimilarity {
+
+	/* * Complete the function below.
+
+	*/ static int stringSimilarity(String[] inputs) { 
+		int[] recur = new int[inputs.length]; 
+		int count =0; int index = 0; 
+		for(int i = 0; i < inputs.length; i ++) { 
+			for(int j = 1; j < inputs.length; j++) { 
+				if( inputs[i].contentEquals(inputs[j]) )
+				{ count++; } 
+				recur[index++] = count; }
+			}
+		int total = 0;
+		for(int c : recur) {
+			total++;
+		}
+		return total;
+	}
+	
+	public static void main(String args) {
+		
+	}
+
+}
